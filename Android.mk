2b13ff5f1b4e8900e18fc6abcc46a1ecb1579053
James Muir <james.muir@graphitesoftware.com>
2017-10-23 03:00:20 -0400

    Fix class loader errors

--- Android.mk
+++ Android.mk
@@ -18,7 +18,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := TimeKeep
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
-
+LOCAL_DEX_PREOPT := nostripping
 LOCAL_PROGUARD_ENABLED := disabled
 
 include $(BUILD_PACKAGE)
